c4916b70cf9dfc84c22e2a8687655dbd3d247dee
Samuel Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-03-24 00:24:46 -0400

    initial commit

--- v2.0/README.md
+++ v2.0/README.md
@@ -1,4 +1,5 @@
-# Massenergize Interactive Website Portal (Front End)
+# Massenergize Portal Website UI Version 2
+
 This is the code for the front end of the interactive portal for Massenergize
 
 ### Maintenance
---
+++ v2.0/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hey! <code>Welcome</code> to the Portal
+          </p>
+          <a
+            className="App-link"
+            href="https://massenergize.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Massenergize Portal Coming Soon
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
