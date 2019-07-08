2792681e28221416e99e35a6ec6d6d31c8515960
Samuel Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-07-07 23:59:32 -0400

    adding firebase config

---
+++ v2.0/src/pages/LoginPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class LoginPage extends Component {
+
+  componentDidMount(){}
+
+  render(){
+    return (
+      <div>
+        <h1>Welcome to the Login Page</h1>
+      </div>
+  );
+  }
+}
