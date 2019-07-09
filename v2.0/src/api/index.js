8b1b756c25b9b96ede0347394a5f27e389ec38aa
Kieran O'Day <Kieran@Kierans-MacBook-Pro.local>
2019-07-09 10:50:23 -0400

    homepagefixed

--- v2.0/src/api/index.js
+++ v2.0/src/api/index.js
@@ -1,45 +1,26 @@
-var apiurl = 'http://localhost:8000/user/'
+import CONST from '../Components/Constants'
 //import { homePageData } from './data/homePageData.js';
-export function getHomePageData(){
-  return fetch(apiurl).then(data => {
-    return data.json()
-  })
-  .then(myJson => {
-    console.log(myJson.homePageData);
-    return myJson.homePageData;
-  }).catch(error => {
-    console.log(error);
-    return {};
+export const sendToBackEnd = (dataToSend, destinationUrl) => {
+  fetch(`${CONST.URL.USER}/auth/csrf`, {
+    method: 'GET',
+    credentials: 'include',
+  }).then(response => response.json()).then(jsonResponse => {
+    const { csrfToken } = jsonResponse.data;
+    return fetch(destinationUrl, {
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'X-CSRFToken': csrfToken
+      },
+      body: dataToSend
+    })
+      .then(response => {
+        console.log(response);
+        return response.json();
+      }).then(data => {
+        console.log(data);
+      });
   });
 }
-// function getIconBoxesData(){
-//   getHomePageData().then(response => {
-//     return response.iconBoxesData;
-//   }).catch(error=>{
-//     console.log(error);
-//   })
-// }
-// function getNavLinks(){
-//   getHomePageData().then(response => {
-//     return response.navLinks;
-//   }).catch(error=>{
-//     console.log(error);
-//   })
-// }
-// function getGraphsData(){
-//   console.log(getHomePageData());
-//   return getHomePageData().graphsData;
-// }
-// function getWelcomeImagesData(){
-//   return getHomePageData().welcomeImages;
-// }
-// function getEventsData(){
-//   return getHomePageData().eventsData;
-// }
-// function getFooterData(){
-//   return getHomePageData().footerData;
-// }
-
-// export {
-//   getHomePageData
-// }
