91a74d2d531a4453e8a4a7815d0cf75e9dd7dec6
Mingle Li <limingle5@gmail.com>
2019-07-12 17:26:53 -0400

    Services page

--- v2.0/src/App.js
+++ v2.0/src/App.js
@@ -12,6 +12,7 @@ import HomePage from './Components/Pages/HomePage/HomePage'
 import ActionsPage from './Components/Pages/ActionsPage/ActionsPage'
 import OneActionPage from './Components/Pages/ActionsPage/OneActionPage'
 import AboutUsPage from './Components/Pages/AboutUsPage/AboutUsPage'
+import ServicesPage from './Components/Pages/ServicesPage/ServicesPage'
 import StoriesPage from './Components/Pages/StoriesPage/StoriesPage'
 import LoginPage from './Components/Pages/LoginPage/LoginPage'
 import EventsPage from './Components/Pages/EventsPage/EventsPage'
@@ -70,6 +71,7 @@ class App extends Component {
 					<Route path="/home" component={HomePage} />
 					<Route exact path="/actions" component={ActionsPage}/>
 					<Route path="/aboutus" component={AboutUsPage}/>
+					<Route path="/services" component={ServicesPage}/>
 					<Route path="/actions/:id" component={OneActionPage}/>
 					<Route path="/testimonials" component={StoriesPage}/>
 					<Route path="/teams" component={TeamsPage}/>
