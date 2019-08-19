0bfa8b9c99cf0869b63048fce76e8307b34fce15
Kieran O'Day <Kieran@Kierans-MBP.fios-router.home>
2019-08-19 16:31:01 -0400

    deleting profile setup but not complete

--- v2.0/src/Components/Pages/ProfilePage/EditingProfileForm.js
+++ v2.0/src/Components/Pages/ProfilePage/EditingProfileForm.js
@@ -16,6 +16,8 @@ class EditingProfileForm extends React.Component {
         this.state = {
             full_name: props.full_name ? props.full_name : '',
             preferred_name: props.preferred_name ? props.preferred_name : '',
+            delete_account: false,
+            are_you_sure: false,
         };
 
         this.onChange = this.onChange.bind(this);
@@ -24,13 +26,51 @@ class EditingProfileForm extends React.Component {
     render() {
         return (
             <form onSubmit={this.onSubmit}>
-                <p>Full Name <span className="text-danger" >*</span></p>
+                {this.state.error? 
+                <p className='text-danger'>{this.state.error}</p> : null
+            }
                 <input type="text" name="full_name" value={this.state.full_name} onChange={this.onChange} required />
-                <p>Preffered Name <span className="text-danger">*</span></p>
+                <p>Full Name <span className="text-danger" >*</span></p>
+
                 <input type="text" name="preferred_name" value={this.state.preferred_name} onChange={this.onChange} required />
-                
+                <p>Preffered Name <span className="text-danger">*</span></p>
+
+
+                <input
+                    type="checkbox"
+                    id="delete_account"
+                    checked={this.state.deleteAccount}
+                    onChange={() => this.setState({ delete_account: !this.state.delete_account })}
+                    style={{ display: "inline-block" }}
+                />
+                <label htmlFor="delete_account" style={{ display: "inline-block" }}> Delete Account</label>
+                <br />
+                {this.state.delete_account ?
+                    <>
+                        <p> Are you sure you want to delete your account? </p>
+
+                        <input
+                            type="radio"
+                            id="yes_im_sure"
+                            checked={this.state.are_you_sure}
+                            onChange={() => this.setState({ are_you_sure: !this.state.are_you_sure })}
+                            style={{ display: "inline-block" }}
+                        />
+                        <label htmlFor="yes_im_sure" style={{ display: "inline-block" }}> Yes </label>
+                        &nbsp;
+                        <input
+                            type="radio"
+                            id="nope_not_sure"
+                            checked={!this.state.are_you_sure}
+                            onChange={() => this.setState({ are_you_sure: !this.state.are_you_sure })}
+                            style={{ display: "inline-block" }}
+                        />
+                        <label htmlFor="nope_not_sure" style={{ display: "inline-block" }}> No</label>
+                        <br />
+                    </> : null
+                }
                 <button className="thm-btn bg-cl-1" type="submit">{"Submit"}</button>
-                <button className="thm-btn red" type='button' onClick = {this.props.closeForm}> Cancel </button> 
+                <button className="thm-btn red" type='button' onClick={this.props.closeForm}> Cancel </button>
             </form>
         );
     }
@@ -46,22 +86,28 @@ class EditingProfileForm extends React.Component {
 
     onSubmit = (event) => {
         event.preventDefault();
-    
-        const body = {
-            "full_name": this.state.full_name,
-            "preferred_name": this.state.preferred_name,
-        }
-        var postURL = URLS.USER + "/" + this.props.user.id;
-        /** Collects the form data and sends it to the backend */
-        postJson(postURL, body).then(json => {
-            console.log(json);
-            if (json.success && json.data) {
-                this.props.reduxLogin(json.data);
-                this.props.closeForm();
+        if (this.state.delete_account && this.state.are_you_sure) {
+            this.deleteAccount();
+        } else {
+            const body = {
+                "full_name": this.state.full_name,
+                "preferred_name": this.state.preferred_name,
             }
-        }).catch(error => {
-            console.log(error);
-        })
+            var postURL = URLS.USER + "/" + this.props.user.id;
+            /** Collects the form data and sends it to the backend */
+            postJson(postURL, body).then(json => {
+                console.log(json);
+                if (json.success && json.data) {
+                    this.props.reduxLogin(json.data);
+                    this.props.closeForm();
+                }
+            }).catch(error => {
+                console.log(error);
+            })
+        }
+    }
+    deleteAccount(){
+        this.setState({error: "Sorry, we don't support deleting accounts yet"});
     }
 }
 
@@ -71,6 +117,6 @@ const mapStoreToProps = store => {
         user: store.user.info
     }
 }
-export default connect(mapStoreToProps, {reduxLogin})(EditingProfileForm);
+export default connect(mapStoreToProps, { reduxLogin })(EditingProfileForm);
 
 
