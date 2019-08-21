9388c9c0b32d7cf9640d7b9203bb9c5eebe04f5c
Kieran O'Day <Kieran@Kierans-MBP.fios-router.home>
2019-08-21 15:54:11 -0400

    another option for welcome images

--- v2.0/src/Components/Shared/WelcomeImages.js
+++ v2.0/src/Components/Shared/WelcomeImages.js
@@ -2,22 +2,22 @@ import React from 'react'
 
 /** Renders an image banner that has one, two or three images across it based on screen size
  */
-class WelcomeImages extends React.Component{
+class WelcomeImages extends React.Component {
     componentDidMount() {
         window.addEventListener('resize', this.handleResize);
     }
-    componentWillUnmount(){
+    componentWillUnmount() {
         window.removeEventListener('resize', this.handleResize);
     }
     handleResize = () => {
         this.forceUpdate();
     };
-    
+
     render(){
         //works best with tall images from online, my images are too big(2-5mb) so they slow it down but these are good
         var picture1 = this.props.data[0].image.url;
         var picture2 = this.props.data[1].image.url;
-        var picture3 = this.props.data[2].image.url;
+        //var picture3 = this.props.data[2].image.url;
 
         var bannerstyle; //checks the width and changes how many images are displayed based on that
         if(window.innerWidth > 1100){
@@ -50,5 +50,41 @@ class WelcomeImages extends React.Component{
             </div>
         );
     }
+
+    // render() {
+    //     var picture1 = this.props.data[0].image.url;
+    //     var picture2 = this.props.data[1].image.url;
+    //     var background1 = {
+    //         backgroundImage: `url(${picture1})`,
+    //         backgroundSize: "cover",
+    //         backgroundPosition: "left top"
+    //     }
+    //     var background2 = {
+    //         backgroundColor: 'red',
+    //     }
+    //     var background3 = {
+    //         backgroundImage: `url(${picture2})`,
+    //         backgroundSize: "cover",
+    //         backgroundPosition: "left top"
+    //     }
+    //     return (
+    //         <div className="inner-banner text-center" style={{height: '200px'}}>
+    //             <div className="container">
+    //                 <div className="row">
+    //                     <div className='col-lg-4 d-lg-block d-md-none' style={background1}>
+    //                     <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    //                     </div>
+    //                     <div className='col-lg-4 text-center' style={background2}>
+    //                     <br/><br/><br/><br/><br/><h1>{this.props.title}</h1><br/><br/><br/><br/><br/>
+    //                     </div>
+    //                     <div className='col-lg-4 d-lg-block d-md-none' style={background3}>
+    //                     <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     );
+
+    // }
 }
 export default WelcomeImages;
