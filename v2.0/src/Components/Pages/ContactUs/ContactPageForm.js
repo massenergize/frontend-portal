384b99680253de743b427be357c7afac0d49a088
pongo1 <mrfimpong@gmail.com>
2019-11-09 04:24:15 +0400

    Extra Contact page touches

--- v2.0/src/Components/Pages/ContactUs/ContactPageForm.js
+++ v2.0/src/Components/Pages/ContactUs/ContactPageForm.js
@@ -21,10 +21,34 @@ class ContactPageForm extends Component {
     let prevContent = this.state.content;
     this.setState({content:{...prevContent,[event.target.name]:event.target.value}})
   }
+  clear(){
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("message").value = "";
+  }
   sendMessage = ()=>{
-    //let spin = document.getElementById('s_spin');
+    let spin = document.getElementById('s_spin');
+    spin.style.display = "block";
     let data = {community_id:this.props.community_id,...DEFAULTS,...this.state.content};
-    console.log("I am all done", data);
+    const msg = this.state.content.message;
+    const name = this.state.content.name;
+    if(!msg || !name){
+      alert("Please provide a message, and name!")
+      spin.style.display = "none";
+      return 
+    }
+    apiCall("admins.message",data).then(res=>{
+     console.log(res);
+     if(res.sucess){
+       spin.style.display = "none";
+       this.clear();
+     }
+     spin.style.display = "none";
+   }).catch(err =>{
+    spin.style.display = "none";
+     console.log(err);
+   });
   }
   render() {
     // const { admins } = this.props;
@@ -40,11 +64,11 @@ class ContactPageForm extends Component {
             <option>All</option>
             {mapped}
           </select> */}
-          <input onChange = {(event)=>{this.handleText(event)}} type="text" name="name" placeholder="Your Name" className="form-control m-textbox" />
-          <input onChange = {(event)=>{this.handleText(event)}}type="email" name="email"  placeholder="Your Email" className="form-control m-textbox" />
-          <input onChange = {(event)=>{this.handleText(event)}}type="number" name="phone"  placeholder="Your Phone" className="form-control m-textbox" />
-          <textarea name="message"onChange = {(event)=>{this.handleText(event)}} className="form-control m-textbox" rows="6" placeholder="Your message"></textarea>
-          <small id="s_spin" className='text text-success' style={{display:'none',fontWeight:'700',margin:20}}><i className="fa fa-spinner fa-spin"></i> sending... </small>
+          <input id="name" onChange = {(event)=>{this.handleText(event)}} type="text" name="name" placeholder="Your Name" className="form-control m-textbox" />
+          <input id ="email" onChange = {(event)=>{this.handleText(event)}}type="email" name="email"  placeholder="Your Email" className="form-control m-textbox" />
+          <input id="phone" onChange = {(event)=>{this.handleText(event)}}type="number" name="phone"  placeholder="Your Phone" className="form-control m-textbox" />
+          <textarea id = "message" name="message"onChange = {(event)=>{this.handleText(event)}} className="form-control m-textbox" rows="6" placeholder="Your message"></textarea>
+          <small id="s_spin" className='text text-success pull-left' style={{display:'none',fontWeight:'700',margin:20}}><i className="fa fa-spinner fa-spin"></i> sending... </small>
           <button onClick = {()=>{this.sendMessage()}}className="raise btn btn-success pull-right m-btn">Send Message</button>
         </div>
       </div>
