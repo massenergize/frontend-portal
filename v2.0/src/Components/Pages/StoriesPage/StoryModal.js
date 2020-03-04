719e26be7b8e2e22400cf64aec7c08a49199855c
pongo1 <mrfimpong@gmail.com>
2020-03-04 13:32:00 +0400

    continuation, phone issues back and forth.. lool

--- v2.0/src/Components/Pages/StoriesPage/StoryModal.js
+++ v2.0/src/Components/Pages/StoriesPage/StoryModal.js
@@ -12,18 +12,18 @@ class StoryModal extends Component {
   render() {
     return (
       <div>
-        <div className="modal-box z-depth-2"  style={{height:630,top:'19%'}}>
-          <h4 className=" modal-close-x round-me" onClick = {()=>{this.props.close()}}><span className="fa fa-close"></span></h4>
+        <div className="modal-box z-depth-2 mob-modal-card"  style={{height:630,top:'19%'}}>
+          <h4 className=" modal-close-x mob-modal-close-x round-me" onClick = {()=>{this.props.close()}}><span className="fa fa-close"></span></h4>
           <center>
-            <h5 style={{ textTransform: 'capitalize' }}>{this.props.content.title}</h5>
+            <h5 className="mob-modal-tittle" style={{ marginBottom:-10, textTransform: 'capitalize' }}>{this.props.content.title}</h5>
             <div>
               {!this.props.content.image ?
-                <img className="testi-green-monster" src={leafy} alt="IMG"/>
+                <img className="testi-green-monster mob-modal-pic-tweak " src={leafy} alt="IMG"/>
                 :
-                <img className="testi-modal-pic" src={this.props.content.image.url} alt="IMG"/>
+                <img className="testi-modal-pic  mob-modal-pic-tweak" src={this.props.content.image.url} alt="IMG"/>
               }
               <div style={{marginTop:30 ,maxHeight:610,overflowY:'scroll'}}>
-              <p >{this.props.content.desc}</p>
+              <p className="mob-modal-p">{this.props.content.desc}</p>
               </div>
             </div>
           </center>
