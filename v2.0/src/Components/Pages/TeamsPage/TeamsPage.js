aed1353609e2ef3ff9662c10128e407fa059ffc0
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2020-06-01 16:58:30 -0400

    Merge branch 'development' into josh-teams-redesign

--- v2.0/src/Components/Pages/TeamsPage/TeamsPage.js
+++ v2.0/src/Components/Pages/TeamsPage/TeamsPage.js
@@ -336,14 +336,6 @@ class TeamsPage extends React.Component {
       teamsSorted[i]["avrgActionsPerMember"] = avrg;
     }
 
-    //AD-HOC! This is for demo-ing JCAN so "Congregation Beth El" shows up first
-    teamsSorted = teamsSorted.sort((a, b) => {
-      if (a.team.name.toLowerCase() > b.team.name.toLowerCase()) return -1;
-      if (a.team.name.toLowerCase() < b.team.name.toLowerCase()) return 1;
-      return 0;
-    });
-
-
     //"force-listen" to the user requested sort
     teamsSorted = this.state.sorted_Teams
       ? this.state.sorted_Teams
