885259aa4820265cf2e909842a9e08af803c2c49
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2020-10-27 11:26:52 -0400

    initialize

--- v2.0/src/config/index.js
+++ v2.0/src/config/index.js
@@ -1,3 +1,3 @@
 var json = require('./config.json');
-const { IS_LOCAL, IS_PROD, IS_SANDBOX, BUILD_VERSION } = json
-module.exports = { IS_LOCAL, IS_PROD, IS_SANDBOX, BUILD_VERSION }
+const { IS_LOCAL, IS_PROD, IS_CANARY, IS_SANDBOX, BUILD_VERSION } = json
+module.exports = { IS_LOCAL, IS_PROD, IS_CANARY, IS_SANDBOX, BUILD_VERSION }
