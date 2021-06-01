9d4686edf8873f041fc58f3e7479800469540493
Brad Hubbard-Nelson <bradhn@mindspring.com>
2021-06-01 16:06:40 -0400

    Grammar

--- v2.0/src/Components/Pages/RegisterPage/RegisterForm.js
+++ v2.0/src/Components/Pages/RegisterPage/RegisterForm.js
@@ -288,7 +288,7 @@ class RegisterFormBase extends React.Component {
               style={{ textDecoration: "underline" }}
               to={this.props.links.signin}
             >
-              I have an profile already
+              I have a profile already
             </Link>
           </p>{" "}
         </div>
