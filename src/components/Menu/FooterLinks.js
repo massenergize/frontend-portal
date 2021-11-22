347091359b5453c7238ef3b10e4652215b069bed
Brad Hubbard-Nelson <bradhn@mindspring.com>
2021-11-21 20:39:00 -0500

    #769 - Clean up footer links
    Database change plus single column

--- src/components/Menu/FooterLinks.js
+++ src/components/Menu/FooterLinks.js
@@ -25,34 +25,56 @@ class FooterLinks extends React.Component {
 	}
 	render() {
 		const links = this.props.links;
-		var half_length = Math.ceil(links.length / 2);
-		var leftSide = links.slice(0, half_length);
-		var rightSide = links.slice(half_length, links.length);
-		return (
-			<div className="col-5 col-md-4">
-				<div className="footer-widget link-column">
-					<div className="section-title d-none d-md-block">
-						<b className="text-white">{this.props.title}</b>
-					</div>
-					<div className="row">
-						<div className="col-12 col-md-6">
-							<div className="widget-content">
-								<ul className="list mb-0 cool-font">
-									{this.renderLinks(leftSide)}
-								</ul>
+		const twoCol = links.length > 4;
+		if (twoCol) {
+			var half_length = Math.ceil(links.length / 2);
+			var leftSide = links.slice(0, half_length);
+			var rightSide = links.slice(half_length, links.length);
+			return (
+				  <div className="col-5 col-md-4">
+					<div className="footer-widget link-column">
+						<div className="section-title d-none d-md-block">
+							<b className="text-white">{this.props.title}</b>
+						</div>
+						<div className="row">
+							<div className="col-12 col-md-6">
+								<div className="widget-content">
+									<ul className="list mb-0 cool-font">
+										{this.renderLinks(leftSide)}
+									</ul>
+								</div>
+							</div>
+							<div className="col-12 col-md-6">
+								<div className="widget-content">
+									<ul className="list mb-0">
+										{this.renderLinks(rightSide)}
+									</ul>
+								</div>
 							</div>
 						</div>
-						<div className="col-12 col-md-6">
-							<div className="widget-content">
-								<ul className="list mb-0">
-									{this.renderLinks(rightSide)}
-								</ul>
-							</div>
-						</div>
 					</div>
+				  </div>
+			);	
+		} else {
+			return (
+				<div className="col-5 col-md-4">
+				  <div className="footer-widget link-column">
+					  <div className="section-title d-none d-md-block">
+						  <b className="text-white">{this.props.title}</b>
+					  </div>
+					  <div className="row">
+						  <div className="col-12 col-md-6">
+							  <div className="widget-content">
+								  <ul className="list mb-0 cool-font">
+									  {this.renderLinks(links)}
+								  </ul>
+							  </div>
+						  </div>
+					  </div>
+				  </div>
 				</div>
-			</div>
-		);
+			);
+		}
 	}
 }
 const mapStoreToProps = (store) => {
