d5dd575984a74a5504b3471bc5bb1592a1c97934
Dave Kinghorn <davidreact@gmail.com>
2021-12-14 16:30:50 -0500

    Added display cookie banner conditional

--- src/components/Shared/CookieBanner.js
+++ src/components/Shared/CookieBanner.js
@@ -3,20 +3,28 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 class CookieBanner extends React.Component {
+	displayCookieBanner = true;
+
 	render() {
 		return (
-			<div className="cookie-banner sticky" style={{background:'#8DC62E', padding:'10px', position:'sticky', bottom:'0', width:'100%'}}>
+			(this.displayCookieBanner ? (<div className="cookie-banner sticky" style={{background:'#8DC62E', padding:'10px', position:'sticky', bottom:'0', width:'100%'}}>
 				<div className="container-fluid">
-					<div className="row">
-						<div className="col-lg-8 col-md-7 col-12">
+					<div className="row ml-auto">
+						<div className="col-lg-9 col-md-9 col-12">
 							<div style={{color:'white', padding:'10px 15px'}}>We use cookies to provide the best experience we can. By using MassEnergize, you accept our cookie policy.</div>
-						</div><div className="col-lg-4 col-md-5 col-12 ml-auto">
+						</div>
+						<div className="col-lg-2 col-md-2 col-12">
 							<button style={{color:'white', padding:'5px 10px'}}>Okay!</button>
-							<button style={{color:'white', padding:'5px 10px'}}>Cookie policy</button>
+							<button href="https://community.massenergize.org/ConcordMA/policies?name=Privacy%20Policy" style={{color:'white', padding:'5px 10px'}}>Cookie policy</button>
+						</div>
+						<div className="col-lg-1 col-md-1 col-12">
+							<button style={{color:'white', padding:'5px 10px'}}>
+								x
+							</button>
 						</div>
 					</div>
 				</div>
-			</div>
+			</div>) : null)
 		);
 	}
 }
