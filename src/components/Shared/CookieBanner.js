e4b38dc1d71b4c33ab407b58a1d055ef47511aca
Dave Kinghorn <davidreact@gmail.com>
2021-12-14 16:37:51 -0500

    Added accept cookies method

--- src/components/Shared/CookieBanner.js
+++ src/components/Shared/CookieBanner.js
@@ -5,6 +5,10 @@ import { connect } from 'react-redux'
 class CookieBanner extends React.Component {
 	displayCookieBanner = true;
 
+	acceptCookies() {
+		this.displayCookieBanner = false;
+	}
+
 	render() {
 		return (
 			(this.displayCookieBanner ? (<div className="cookie-banner sticky" style={{background:'#8DC62E', padding:'10px', position:'sticky', bottom:'0', width:'100%'}}>
@@ -14,11 +18,11 @@ class CookieBanner extends React.Component {
 							<div style={{color:'white', padding:'10px 15px'}}>We use cookies to provide the best experience we can. By using MassEnergize, you accept our cookie policy.</div>
 						</div>
 						<div className="col-lg-2 col-md-2 col-12">
-							<button style={{color:'white', padding:'5px 10px'}}>Okay!</button>
-							<button href="https://community.massenergize.org/ConcordMA/policies?name=Privacy%20Policy" style={{color:'white', padding:'5px 10px'}}>Cookie policy</button>
+							<button onClick={this.acceptCookies()} style={{color:'white', padding:'5px 10px'}}>Okay!</button>
+							<button link="https://community.massenergize.org/ConcordMA/policies?name=Privacy%20Policy" style={{color:'white', padding:'5px 10px'}}>Cookie policy</button>
 						</div>
 						<div className="col-lg-1 col-md-1 col-12">
-							<button style={{color:'white', padding:'5px 10px'}}>
+							<button onClick={this.acceptCookies()} style={{color:'white', padding:'5px 10px'}}>
 								x
 							</button>
 						</div>
