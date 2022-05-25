58644b27e3ff320684d26cb6f128760809004abe
frimpongopoku <pongofrimi@Gmail.com>
2022-05-25 15:33:57 +0400

    Now ready for un-auth workflow

--- cypress/integration/reusable/cookie-banner-shows.spec.js
+++ cypress/integration/reusable/cookie-banner-shows.spec.js
@@ -1,14 +1,14 @@
 import fields from "../../fixtures/json/fields";
 
 describe("Cookies banner shows up on load", () => {
-  it("Visited homepage", () => cy.visit(fields.urls.homepage));
+  it("Visited homepage", () => cy.visit(fields.urls.homepage.withParams));
 
   it("Saw cookies and privacy policy banner", () => {
-    cy.get("#test-cookie-banner").should("exist");
+    cy.get("#test-cookie-banner-okay").should("exist");
   });
 
   it("Accepted cookie successfully", () => {
-    cy.deactivateTour();
+    // cy.deactivateTour();
     cy.get("#test-cookie-banner-okay").click();
     cy.get("#test-cookie-banner").should("not.exist");
   });
