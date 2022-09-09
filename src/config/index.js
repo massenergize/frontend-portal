2e8666643e32cf78f403c66f749413577c0952f2
Dave Kinghorn <davidreact@gmail.com>
2022-09-09 11:55:24 -0400

    remove module.exports

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,4 @@
 var json = require('./config.json');
 const { IS_LOCAL, IS_PROD, IS_CANARY, BUILD_VERSION } = json;
 // module.exports = { IS_LOCAL, IS_PROD, IS_CANARY, BUILD_VERSION }
-module.exports = IS_LOCAL;
-module.exports = IS_PROD;
-module.exports = IS_CANARY;
-module.exports = BUILD_VERSION;
+export default { IS_LOCAL, IS_PROD, IS_CANARY, BUILD_VERSION };
