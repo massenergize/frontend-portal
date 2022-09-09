248745c66f879bcf1c4614e3d317c4fa4fb8adb9
Dave Kinghorn <davidreact@gmail.com>
2022-09-09 10:53:55 -0400

    Alter exports for config/index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,7 @@
 var json = require('./config.json');
-const { IS_LOCAL, IS_PROD, IS_CANARY, BUILD_VERSION } = json
-module.exports = { IS_LOCAL, IS_PROD, IS_CANARY, BUILD_VERSION }
+const { IS_LOCAL, IS_PROD, IS_CANARY, BUILD_VERSION } = json;
+// module.exports = { IS_LOCAL, IS_PROD, IS_CANARY, BUILD_VERSION }
+module.exports = IS_LOCAL;
+module.exports = IS_PROD;
+module.exports = IS_CANARY;
+module.exports = BUILD_VERSION;
