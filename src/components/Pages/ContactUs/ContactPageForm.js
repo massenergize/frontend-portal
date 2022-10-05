035a09b3e1029602a5b2fb68144be96fc7995339
frimpongopoku <pongofrimi@Gmail.com>
2022-10-05 06:55:04 +0400

    contact us form now works properly

--- src/components/Pages/ContactUs/ContactPageForm.js
+++ src/components/Pages/ContactUs/ContactPageForm.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 import { apiCall } from "../../../api/functions";
-// import MEButton from "../Widgets/MEButton";
-// import METextView from "../Widgets/METextView";
-// import METextField from "../Widgets/METextField";
 import MEFormGenerator from "../Widgets/FormGenerator/MEFormGenerator";
 
 const DEFAULTS = {
@@ -21,54 +18,6 @@ class ContactPageForm extends Component {
     };
     this.onSubmit = this.onSubmit.bind(this);
   }
-
-  // handleText = (event) => {
-  //   let prevContent = this.state.content;
-  //   this.setState({
-  //     content: { ...prevContent, [event.target.name]: event.target.value },
-  //   });
-  // };
-  // clear() {
-  //   document.getElementById("name").value = "";
-  //   document.getElementById("email").value = "";
-  //   document.getElementById("title").value = "";
-  //   document.getElementById("message").value = "";
-  // }
-  // sendMessage = () => {
-  //   let spin = document.getElementById("s_spin");
-  //   spin.style.display = "block";
-  //   const msg = this.state.content.body;
-  //   const name = this.state.content.user_name;
-  //   const title = "Message from " + name;
-  //   let data = {
-  //     community_id: this.props.community_id,
-  //     title,
-  //     ...DEFAULTS,
-  //     ...this.state.content,
-  //   };
-
-  //   if (!msg || !name) {
-  //     alert("Please provide a message, and name!");
-  //     spin.style.display = "none";
-  //     return;
-  //   }
-
-  //   apiCall("admins.messages.add", data)
-  //     .then((res) => {
-  //       alert(
-  //         "Thanks for contacting the community administrator. You should receive a response within a few days."
-  //       );
-  //       if (res.success) {
-  //         spin.style.display = "none";
-  //         this.clear();
-  //       }
-  //       spin.style.display = "none";
-  //     })
-  //     .catch((err) => {
-  //       spin.style.display = "none";
-  //       console.log(err);
-  //     });
-  // };
 
   onSubmit(e, content, resetForm) {
     e.preventDefault();
