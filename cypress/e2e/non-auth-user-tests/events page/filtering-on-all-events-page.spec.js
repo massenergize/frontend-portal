96a4af680d428326e2c87b5981d4954f183b7ab7
tahiru <abdullai.tahiru@gmail.com>
2023-03-15 09:47:56 +0000

    fixing event filter test

--- cypress/e2e/non-auth-user-tests/events page/filtering-on-all-events-page.spec.js
+++ cypress/e2e/non-auth-user-tests/events page/filtering-on-all-events-page.spec.js
@@ -7,12 +7,18 @@ describe("Filtering on all events page works well", function () {
   it("Got the title of the first event", function () {
     cy.get(".test-event-card-title")
       .first()
-      .then(($el) => (title = $el.text()));
+      .then(($el) =>{
+        console.log("=== Write title ===", $el.text());
+        return (title = $el.text());
+      });
   });
   it("Typed content into filterbox", () => {
     typeInsideFilterbox(title);
   });
   it("Event cards were filtered", () => {
-    cy.get(".test-event-card-title").each(($el) => $el.text() === title);
+    cy.get(".test-event-card-title").each(($el) => {
+      console.log("=== Checking title match ===", $el.text());
+      return $el.text().includes(title)}
+      );
   });
 });
