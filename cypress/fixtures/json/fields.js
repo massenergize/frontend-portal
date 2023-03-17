eff1077ee35e1b21a1b4764efe4a7e16820b2edd
tahiru <abdullai.tahiru@gmail.com>
2023-03-17 11:12:40 +0000

    introduced test mode

--- cypress/fixtures/json/fields.js
+++ cypress/fixtures/json/fields.js
@@ -1,8 +1,9 @@
 import { IS_LOCAL } from "../../../src/config";
 import URLS from "../../../src/api/urls";
 
+const testMode = process.env.REACT_APP_TEST_MODE || false
 var DOMAIN = URLS["COMMUNITIES"] + "/";
-if (IS_LOCAL) var DOMAIN = "http://localhost:3000/";
+if (IS_LOCAL || testMode) var DOMAIN = "http://localhost:3000/";
 var COMMUNITY = "wayland"; // Only Change this to a community that exists in your DB (wayland exists everywhere, so this should work everywhere)
 const BASE_URL = DOMAIN + COMMUNITY + "/";
 const urlParams = "?tour=false"; // useful, dont remove
