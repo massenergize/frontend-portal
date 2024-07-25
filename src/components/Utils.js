b2476faa1dd24018f9e1a7defdc8c046b4545842
Jephthah <boatengjephthah27@gmail.com>
2024-07-25 14:30:45 +0000

    update Utils.js

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -11,18 +11,6 @@ export const replaceAllOccurrences = (str, oldValue, newValue) => {
 };
 export const addLeadingSlashToLinks = (links) => {
   return links?.map((link) => {
-    if (!link?.link?.startsWith("/") && !isAProperURL(link?.link)) {
-      link.link = "/" + link?.link;
-    }
-    return link;
-  });
-};
-
-export const replaceAllOccurrences = (str, oldValue, newValue) => {
-  return str.replace(new RegExp(oldValue, "g"), newValue);
-};
-export const addLeadingSlashToLinks = (links) => {
-  return links?.map((link) => {
     if (!link?.link?.startsWith("/")) {
       link.link = "/" + link?.link;
     }
